3256b50c8fb0f7338f68baeea675d0062fede915
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-03 17:21:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
 
+  attr_reader :side_1, :side_2, :side_3
+
   def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
   def kind
