d7c298f6b82845713296f4f0bb0c75c9abac1726
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-03 17:24:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,10 +9,13 @@ class Triangle
   end
 
   def kind
-    
+    if [side_1, side_2, side_3].any?(|side| side <= 0) 
+      raise TriangleError
   end
 
   class TriangleError < StandardError
-
+    def message
+      "Not a valid type of trinagle."
+    end
   end
 end
